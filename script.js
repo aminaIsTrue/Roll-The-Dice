be0787d5e84b05bed3ef859655061477daf1ea8e
aminaIsTrue <aamina.ouj@gmail.com>
2022-07-23 12:08:04 +0200

    fix load image

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ btnRoll.addEventListener('click', function () {
     // 2. Display dice
     diceEl.classList.remove('hidden');
 
-    diceEl.src = `./img/dice-${dice}.44e9dd05.png`;
+    diceEl.src = `dice-${dice}.png`;
 
     // 3. Check for rolled 1
     if (dice !== 1) {
