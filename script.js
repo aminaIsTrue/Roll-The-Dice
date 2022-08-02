60bc901516f45301e0c4cb22abbbcb9bcaa4e78a
aminaIsTrue <aamina.ouj@gmail.com>
2022-08-02 13:00:03 +0200

    fix modal window

--- script.js
+++ script.js
@@ -111,7 +111,7 @@ btnNew.addEventListener('click', init);
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
-const btnsOpenModal = document.querySelectorAll('.show-modal');
+const btnsOpenModal = document.querySelector('.show-modal');
 
 const openModal = function () {
   modal.classList.remove('hidden');
@@ -123,15 +123,12 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.addEventListener('click', openModal);
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
 document.addEventListener('keydown', function (e) {
-  // console.log(e.key);
-
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
